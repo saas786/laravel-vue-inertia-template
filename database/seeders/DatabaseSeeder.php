a78f347081bff8e111551a8b0b68459639c7d0e5
Matheus Caetano <caetanomatheus876@gmail.com>
2021-04-11 17:24:10 -0300

    Create user seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -9,6 +10,6 @@ class DatabaseSeeder extends Seeder
     /** Seed the application's database.*/
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
     }
 }
